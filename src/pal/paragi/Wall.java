7be88145dba8207bd1f8755b0c40779b393fc05b
paragip <pal.paragi@gmail.com>
2022-06-17 20:24:41 +0200

    constructor exercise - wall area

---
+++ src/pal/paragi/Wall.java
@@ -0,0 +1,57 @@
+package pal.paragi;
+
+public class Wall {
+
+    private double width;
+    private double height;
+
+    public Wall() {
+    }
+
+    public Wall(double width, double height) {
+
+        if (width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getArea() {
+        return getHeight() * getWidth();
+    }
+
+
+
+}
